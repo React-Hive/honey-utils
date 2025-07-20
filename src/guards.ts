a63ce4ae206e29a79a51446d6095009988f38d40
Mykhailo Aliinyk <m.aliynik@gmail.com>
2025-07-20 19:03:43 +0100

    1.1.0 - Add new type guard utility functions

--- src/guards.ts
+++ src/guards.ts
@@ -1,8 +1,15 @@
 export const isString = (value: unknown): value is string => typeof value === 'string';
 
+export const isNumber = (value: unknown): value is number => typeof value === 'number';
+
+export const isBool = (value: unknown): value is boolean => typeof value === 'boolean';
+
 export const isObject = (value: unknown): value is object => typeof value === 'object';
 
 export const isFunction = (value: unknown) => typeof value === 'function';
+
+export const isPromise = <T = unknown>(value: unknown): value is Promise<T> =>
+  isFunction((value as Promise<T>)?.then);
 
 /**
  * Checks if a value is null or undefined.
@@ -13,3 +20,18 @@ export const isFunction = (value: unknown) => typeof value === 'function';
  */
 export const isNil = (value: unknown): value is null | undefined =>
   value === undefined || value === null;
+
+/**
+ * Checks whether the provided value is considered "empty".
+ *
+ * A value is considered empty if it is:
+ * - `null`
+ * - `undefined`
+ * - `''`
+ *
+ * @param value - The value to check.
+ *
+ * @returns `true` if the value is empty; otherwise, `false`.
+ */
+export const isNilOrEmptyString = (value: unknown): value is null | undefined =>
+  value === '' || isNil(value);
